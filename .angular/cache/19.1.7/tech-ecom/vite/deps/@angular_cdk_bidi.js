bd04474d46acfffa5ed65e1663f7a5e9f0329775
danielviana10 <danielvianna43@gmail.com>
2025-02-15 01:27:12 -0300

    adiciona novos arquivos de ativos e configurações ao projeto

---
+++ .angular/cache/19.1.7/tech-ecom/vite/deps/@angular_cdk_bidi.js
@@ -0,0 +1,14 @@
+import {
+  BidiModule,
+  DIR_DOCUMENT,
+  Dir,
+  Directionality
+} from "./chunk-ZFIBK3MD.js";
+import "./chunk-3SAYUQXN.js";
+import "./chunk-PCBEJI2G.js";
+export {
+  BidiModule,
+  DIR_DOCUMENT,
+  Dir,
+  Directionality
+};
